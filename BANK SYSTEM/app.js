396a3a469c642c3cf667b1aa77ea1b4e7b9d93f7
ChamathJ <chamathmmw@gmail.com>
2023-10-11 15:02:36 +0530

    Static files are moved to public folder and enabled use of public folder in app.js

--- BANK SYSTEM/app.js
+++ BANK SYSTEM/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000; // A reminder to change this to environment variables before deploying
 
+app.use(express.static("public"));
+
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/main-page.html");
 });
