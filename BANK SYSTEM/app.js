decbc3c784d5e1a2d56462e3b2a09cfbb2264e86
ChamathJ <chamathmmw@gmail.com>
2023-10-11 14:24:01 +0530

    Initial stage of bank app

---
+++ BANK SYSTEM/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const port = 3000; // A reminder to change this to environment variables before deploying
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/main-page.html");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port,()=>{
+    console.log("Server Running");
+});
