237f7f99783fc1037e45df7e0ce6fea72ed53a2a
Chalana Gayan <gayanchalana@gmail.com>
2023-10-19 22:54:36 +0530

    required settings

--- BANK SYSTEM/app.js
+++ BANK SYSTEM/app.js
@@ -4,8 +4,17 @@ const app = express();
 const hbs = require("hbs");
 app.use(bodyParser.urlencoded({extended:true}));
 
-//Setup template engine
+const path = require('path');  // Require the path module
+
+// Set the views directory to the correct path
+app.set('views', path.join(__dirname, 'views'));
+
+// Assuming 'ejs' is the view engine you are using, set it up
 app.set('view engine', 'html');
+
+
+//Setup template engine
+// app.set('view engine', 'html');
 app.engine('html', require('hbs').__express);
 
 const port = 3000; // A reminder to change this to environment variables before deploying
--- BANK SYSTEM/queries/query.js
+++ BANK SYSTEM/queries/query.js
@@ -3,19 +3,29 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+// ------------------------------------
+// chalana's database connection settings
+// ------------------------------
 const con = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PWD,
-    database: process.env.MYSQL_DATABASE
-});
+    host: 'localhost',
+    user: 'root',
+    password: 'Chalana1234',
+    database: 'bank_system',
+    port: 3306
+  });
+
+// ------------------------------
+
+// const con = mysql.createConnection({
+//     host: process.env.MYSQL_HOST,
+//     user: process.env.MYSQL_USER,
+//     password: process.env.MYSQL_PWD,
+//     database: process.env.MYSQL_DATABASE
+// });
 
 con.connect((err)=>{
     console.log(err);
 })
-
-
-
 
 exports.checkUser = (req,res)=>{
     let username = req.body.username;
